41cae7277b002720ed1f4e3b6acf59a922e52a03
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-06-26 12:45:05 +0530

    JSON and XML Reader Writer Examples

--- item-readers/src/main/java/com/howtodoinjava/demo/jsonReaderWriter/JsonReaderJobConfig.java
+++ item-readers/src/main/java/com/howtodoinjava/demo/jsonReaderWriter/JsonReaderJobConfig.java
@@ -1,37 +1,40 @@
-package com.howtodoinjava.demo.xmlFileReader;
+package com.howtodoinjava.demo.jsonReaderWriter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.howtodoinjava.demo.model.Person;
 import com.howtodoinjava.demo.processor.LoggingPersonProcessor;
-import com.howtodoinjava.demo.writer.LoggingPersonWriter;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.xml.StaxEventItemReader;
-import org.springframework.batch.item.xml.StaxEventItemWriter;
-import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
-import org.springframework.batch.item.xml.builder.StaxEventItemWriterBuilder;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonFileItemWriter;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.text.SimpleDateFormat;
+
 @Configuration
 @SpringBootApplication
-public class XmlReaderJobConfig {
+public class JsonReaderJobConfig {
 
-  @Value("classpath:person.xml")
-  private Resource xmlFile;
+  @Value("classpath:person.json")
+  private Resource jsonFile;
 
   @Bean
   Job job(Step step1, JobRepository jobRepository) {
@@ -43,8 +46,8 @@ public class XmlReaderJobConfig {
   }
 
   @Bean
-  public Step step1(StaxEventItemReader<Person> reader,
-                    StaxEventItemWriter<Person> writer,
+  public Step step1(JsonItemReader<Person> reader,
+                    JsonFileItemWriter<Person> writer,
                     JobRepository jobRepository,
                     PlatformTransactionManager txManager) {
 
@@ -59,39 +62,35 @@ public class XmlReaderJobConfig {
 
   @Bean
   @StepScope
-  public StaxEventItemReader<Person> personXmlFileReader() {
-    return new StaxEventItemReaderBuilder<Person>()
-        .name("personXmlFileReader")
-        .resource(xmlFile)
-        .addFragmentRootElements("person")
-        .unmarshaller(personMarshaller())
+  public JsonItemReader<Person> personJsonItemReader() {
+
+    /*ObjectMapper objectMapper = new ObjectMapper();
+    //customize objectMapper if needed
+    JacksonJsonObjectReader<Person> jsonObjectReader = new JacksonJsonObjectReader<>(Person.class);
+    jsonObjectReader.setMapper(objectMapper);*/
+
+    return new JsonItemReaderBuilder<Person>()
+        .name("personJsonItemReader")
+        .jsonObjectReader(new JacksonJsonObjectReader<>(Person.class))
+        .resource(jsonFile)
         .build();
   }
 
   @Bean
-  public StaxEventItemWriter<Person> personXmlFileWriter() {
+  public JsonFileItemWriter<Person> personJsonFileItemWriter() {
 
     PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-    WritableResource outputXml = (WritableResource) resolver.getResource("file:output-person.xml");
+    WritableResource outputJsonFile = (WritableResource) resolver.getResource("file:output-person.json");
 
-    return new StaxEventItemWriterBuilder<Person>()
-        .name("personXmlFileWriter")
-        .marshaller(personMarshaller())
-        .resource(outputXml)
-        .rootTagName("people")
-        .overwriteOutput(true)
+    return new JsonFileItemWriterBuilder<Person>()
+        .name("personJsonFileItemWriter")
+        .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+        .resource(outputJsonFile)
         .build();
 
-  }
-
-  @Bean
-  public Jaxb2Marshaller personMarshaller() {
-    Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-    jaxb2Marshaller.setClassesToBeBound(Person.class);
-    return jaxb2Marshaller;
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(XmlReaderJobConfig.class);
+    SpringApplication.run(JsonReaderJobConfig.class);
   }
 }
